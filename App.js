996265035c45bb52a965e087b87e1147e624e42d
Anro <anelnichenko@bk.ru>
2023-11-04 17:05:16 +0300

    added circle and text

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.back}>
+        <View style={styles.cycle}>
+
+        </View>
+        <Text style={styles.text}>Твой умный офис</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  cycle:{
+    backgroundColor: '#222222',
+    color: '#222222',
+    width: '130%',
+    height: 550,
+    borderWidth: 1,
+    borderRadius: 500,
+    top: -120,
+  },
+  back:{
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#8ACCB0',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text:{
+    color: 'black',
+    fontSize: 30,
+    fontFamily: 'Montserrat Bold',
   },
 });
