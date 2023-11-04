1d02ec461a892951984ecc4e0aeed52dca8887a8
Anro <anelnichenko@bk.ru>
2023-11-04 18:09:53 +0300

    загрузка лого, шрифтов, создание страниц входа и регистрации

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import * as Font from 'expo-font';
+//import AppLoading from 'expo-app-loading';
+
+const fonts = () => Font.loadAsync({
+  'mt-bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+  'mt-italic': require('./assets/fonts/Montserrat-Italic.ttf'),
+  'mt-medium': require('./assets/fonts/Montserrat-Medium.ttf'),
+  'mt-regular': require('./assets/fonts/Montserrat-Regular.ttf'),
+  'mt-semibold': require('./assets/fonts/Montserrat-SemiBold.ttf'),
+  'ko-regular': require('./assets/fonts/KronaOne-Regular.ttf')
+})
 
 export default function App() {
+  const [font, setFont] = useState(false);
   return (
     <View style={styles.back}>
         <View style={styles.cycle}>
-
+          <Image source={require('./assets/logo.png')}/>
         </View>
         <Text style={styles.text}>Твой умный офис</Text>
     </View>
@@ -21,7 +33,7 @@ const styles = StyleSheet.create({
     height: 550,
     borderWidth: 1,
     borderRadius: 500,
-    top: -120,
+    top: -160,
   },
   back:{
     width: '100%',
@@ -32,6 +44,6 @@ const styles = StyleSheet.create({
   text:{
     color: 'black',
     fontSize: 30,
-    fontFamily: 'Montserrat Bold',
+    fontFamily: 'mt-bold',
   },
 });
