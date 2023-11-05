2637750912bf3d3e1bd1246363d1033249c1c2e0
Anro <anelnichenko@bk.ru>
2023-11-05 17:59:02 +0300

    добавлена блочная структура на окно приветствия

--- components/Main.js
+++ components/Main.js
@@ -8,45 +8,58 @@ export default function Main({ navigation }) {
 
     return (
         <View style={styles.back}>
-            <View style={styles.cycle}>
-                <Image style={styles.img} source={require('../assets/logo.png')} />
+            <View style={styles.div}>
+                <View style={styles.cycle}>
+                    <Image style={styles.img} source={require('../assets/logo.png')} />
+                </View>
             </View>
-            <Text style={styles.text}>Твой умный офис</Text>
-            
-            
-            <TouchableWithoutFeedback onPress={loadScene('SignIn')}>
-                <View style={styles.button}><Text style={styles.textButton}>Вход</Text></View>
-            </TouchableWithoutFeedback>
-
-            <TouchableWithoutFeedback onPress={loadScene('SignUp')}>
-                <View style={styles.button}><Text style={styles.textButton}>Регистрация</Text></View>
-            </TouchableWithoutFeedback>
+            <View style={styles.div1}>
+                <Text style={styles.text}>Твой умный офис</Text>
+                <TouchableWithoutFeedback onPress={loadScene('SignIn')}>
+                    <View style={styles.button}><Text style={styles.textButton}>Вход</Text></View>
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback onPress={loadScene('SignUp')}>
+                    <View style={styles.button}><Text style={styles.textButton}>Регистрация</Text></View>
+                </TouchableWithoutFeedback>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    div:{
+        width: '100%',
+        alignItems: 'center',
+    },
+    div1:{
+        height: '50%',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
     img: {
         top: 40,
         width: 250,
         height: 250,
+        top: -30,
     },
     cycle: {
         backgroundColor: '#222222',
         color: '#222222',
         width: '130%',
-        height: 550,
+        height: 390,
         borderWidth: 1,
-        borderRadius: 500,
+        borderBottomLeftRadius: 390,
+        borderBottomRightRadius: 390,
         alignItems: 'center',
         justifyContent: 'center',
-        top: -160,
     },
     back: {
         width: '100%',
         height: '100%',
         backgroundColor: '#8ACCB0',
         alignItems: 'center',
+        justifyContent: 'flex-start',
     },
     text: {
         color: 'black',
